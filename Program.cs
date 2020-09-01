64a87f143bcd487ebd7f8a7a077f6d685b6032e8
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-01 10:51:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace TE18B_Filsystem
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      // File.WriteAllText("words.txt", "Thing");
+
+      // string s = File.ReadAllText("words.txt");
+
+      // Console.WriteLine(s);
+
+
+
+      // string[] words = {"Hat", "Coat", "Pants"};
+
+      if (File.Exists("words.txt"))
+      {
+        string[] words = File.ReadAllLines("words.txt");
+
+        Console.WriteLine(words[0]);
+      }
+
+
+
+      // File.WriteAllLines("words.txt", words);
+
+      Console.ReadLine();
+
+    }
+  }
+}
